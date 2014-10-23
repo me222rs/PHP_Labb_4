5a801bfeb1b8cba5f443e9df8ad465ece658fcf3
me222rs <me222rs@student.lnu.se>
2014-10-23 15:52:30 +0200

    Hittat felet
    Hade fel namn på tabellen i mina sql satser. Nu borde den fungera.

--- modelLogin.php
+++ modelLogin.php
@@ -12,35 +12,18 @@
     }
 	
 	public function CheckIfUsernameIsAvailable($username){
-		$connection = mysqli_connect("127.0.0.1", "root", "", "loginlabb4");
+		$connection = mysqli_connect("mikaeledberg.se.mysql", "mikaeledberg_se", "ee8w7zQD", "mikaeledberg_se");
     	if (mysqli_connect_errno($connection)){
         	echo "MySql Error: " . mysqli_connect_error();
     	}
-		$query = mysqli_query($connection,"SELECT * FROM member WHERE username='$username'");
+		$query = mysqli_query($connection,"SELECT * FROM Users WHERE Username='$username'");
 		$count = mysqli_num_rows($query);
     	$row = mysqli_fetch_array($query);
 		
 		if($count == 1){
-			//echo $count;
 			return FALSE;
 		}
 		return TRUE;
-	}
-	
-	public function SuccessUser(){
-		if(isset($_SESSION['successUser'])){
-			$successUser = $_SESSION['successUser'];
-			return $successUser;
-		}
-		return false;
-	}
-	
-	public function SuccessMessage(){
-		if(isset($_SESSION['successMessage'])){
-			$successMessage = $_SESSION['successMessage'];
-			return $successMessage;
-		}
-		return false;
 	}
 	
 	public function UnsetFailUser(){
@@ -59,16 +42,32 @@
 		
 	}
 	
+	public function SuccessUser(){
+		if(isset($_SESSION['successUser'])){
+			$successUser = $_SESSION['successUser'];
+			return $successUser;
+		}
+		return false;
+	}
+	
+	public function SuccessMessage(){
+		if(isset($_SESSION['successMessage'])){
+			$successMessage = $_SESSION['successMessage'];
+			return $successMessage;
+		}
+		return false;
+	}
+	
 	public function Save($newUsername, $newPassword){
 		$newPassword = md5($newPassword);
 		//$this->username = $newUsername;
 		//$this->password = $newPassword;
-		$connection = mysqli_connect("127.0.0.1", "root", "", "loginlabb4");
+		$connection = mysqli_connect("mikaeledberg.se.mysql", "mikaeledberg_se", "ee8w7zQD", "mikaeledberg_se");
     	if (mysqli_connect_errno($connection)){
         	echo "MySql Error: " . mysqli_connect_error();
     	}
 		
-    	mysqli_query($connection,"INSERT member SET Username = '$newUsername', Password = '$newPassword'");
+    	mysqli_query($connection,"INSERT Users SET Username = '$newUsername', Password = '$newPassword'");
 	
 		mysqli_close($connection);
 		$_SESSION['successUser'] = $newUsername;
@@ -84,12 +83,12 @@
 		
 		//$encryptedPassword = md5($password);
 			
-		$connection = mysqli_connect("127.0.0.1", "root", "", "loginlabb4");
+		$connection = mysqli_connect("mikaeledberg.se.mysql", "mikaeledberg_se", "ee8w7zQD", "mikaeledberg_se");
     	if (mysqli_connect_errno($connection)){
         	echo "MySql Error: " . mysqli_connect_error();
     	}
 
-    	$query = mysqli_query($connection,"SELECT * FROM member WHERE username='$username' && password='$password'");
+    	$query = mysqli_query($connection,"SELECT * FROM Users WHERE Username='$username' && Password='$password'");
     	$count = mysqli_num_rows($query);
     	$row = mysqli_fetch_array($query);
 
@@ -134,11 +133,12 @@
         }
         
         public function checkLoginCookie($username,$password){
-        	
+        	//var_dump($username);
+        	//var_dump($password);
             $getCookieTime = file_get_contents('cookieTime.txt');
 			$login = $this->checkLogin($username, $password);
             if ($login == TRUE && $getCookieTime > time()){
-            	
+            	//var_dump($password);
 				$_SESSION["login"] = $username;
 				$_SESSION["checkBrowser"] = $_SERVER['HTTP_USER_AGENT'];
     			return TRUE;
